2f4d5d4c512cbfcbf5ab914f79f7dbf11020c1dd
sidjy <sidjy@users.noreply.github.com>
2016-11-07 10:30:54 +0100

    Update extract_images.pl

--- extract_images.pl
+++ extract_images.pl
@@ -77,6 +77,12 @@ while (my $row = <$fh>) {
 
 
 chdir($video_path) || die "Cannot go to $video_path\n";
+
+# check for subdirs
+unless (-d $video_path."/img/F" && -d $video_path."/img/R") {
+  die "Missing $video_path."/img/F" or $video_path."/img/R" subdirectories\n";
+}
+
 my $data_dir='.'; opendir( DATA_DIR, $data_dir) || die "Cannot open $data_dir\n";
 my @files = sort readdir(DATA_DIR);
 
